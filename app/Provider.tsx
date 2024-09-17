5b308f1233cc94c4cc466bd15e859d7d7c7e6a83
rabin.raut <rabin.raut@globalsquareit.com>
2024-09-17 23:10:22 +0545

    collaborative editor room updates

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,15 +1,19 @@
 'use client';
+
 import Loader from '@/components/Loader';
 import { ClientSideSuspense, LiveblocksProvider } from '@liveblocks/react/suspense';
 import { ReactNode } from 'react';
+import { getClerkUsers } from '@/lib/actions/user.actions';
 
 const Provider = ({ children }: { children: ReactNode }) => {
     return (
         <LiveblocksProvider
             authEndpoint="/api/liveblocks-auth"
-            // resolveUsers={async ({ userIds }) => {
-            //     const users = await getClerkUser();
-            // }}
+            resolveUsers={async ({ userIds }) => {
+                const users = await getClerkUsers({ userIds });
+
+                return users;
+            }}
         >
             <ClientSideSuspense fallback={<Loader />}>
                 {children}
